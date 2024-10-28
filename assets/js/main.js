26d16906f736f7dea2eb5149e91e44c87d696a81
trantuanhung <23659581.hung@student.iuh.edu.vn>
2024-10-29 00:47:13 +0700

    git fix countOfVote

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,26 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getDatabase, ref, push, set, onValue, update, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
 //firebase config a Hung comment demo
-const firebaseConfig = {
-    apiKey: "AIzaSyDFIW4HVIG0usA868ltUJ_eTukLixdoT2A",
-    authDomain: "hcyu-fit.firebaseapp.com",
-    databaseURL: "https://hcyu-fit-default-rtdb.firebaseio.com",
-    projectId: "hcyu-fit",
-    storageBucket: "hcyu-fit.appspot.com",
-    messagingSenderId: "1059484411477",
-    appId: "1:1059484411477:web:83aca0a7b469d1fc5fd6dc"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDFIW4HVIG0usA868ltUJ_eTukLixdoT2A",
+//     authDomain: "hcyu-fit.firebaseapp.com",
+//     databaseURL: "https://hcyu-fit-default-rtdb.firebaseio.com",
+//     projectId: "hcyu-fit",
+//     storageBucket: "hcyu-fit.appspot.com",
+//     messagingSenderId: "1059484411477",
+//     appId: "1:1059484411477:web:83aca0a7b469d1fc5fd6dc"
+// };
 
 //My firebase config 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCAgJndV6rdgtlIpQ5cjuUrL9KSbCS6fTU",
-//     authDomain: "votting-app-52c6c.firebaseapp.com",
-//     databaseURL: "https://votting-app-52c6c-default-rtdb.firebaseio.com",
-//     projectId: "votting-app-52c6c",
-//     storageBucket: "votting-app-52c6c.appspot.com",
-//     messagingSenderId: "868756643395",
-//     appId: "1:868756643395:web:5abc86da00bdfac3d8fef6"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyCAgJndV6rdgtlIpQ5cjuUrL9KSbCS6fTU",
+    authDomain: "votting-app-52c6c.firebaseapp.com",
+    databaseURL: "https://votting-app-52c6c-default-rtdb.firebaseio.com",
+    projectId: "votting-app-52c6c",
+    storageBucket: "votting-app-52c6c.appspot.com",
+    messagingSenderId: "868756643395",
+    appId: "1:868756643395:web:5abc86da00bdfac3d8fef6"
+};
 
 
 // Initialize Firebase
@@ -37,13 +37,15 @@ window.addEventListener("load", function () {
 //End lazy loading
 
 // Hàm để tăng biến count
-const incrementCount = () => {
-    // Lấy giá trị hiện tại của count từ Firebase
-    onValue(countRef, (snapshot) => {
-        let currentCount = snapshot.exists() ? snapshot.val() : 0;
-        // Tăng count lên 1
-        set(countRef, currentCount + 1);
-    }, { onlyOnce: true });
+const addUserVote = () => {
+    const dataUserVote = {
+        usersVote : 1,
+    };
+
+    const newUserRef = push(countRef);
+
+    set(newUserRef, dataUserVote);
+
 };
 // End Hàm để tăng biến count
 
@@ -80,14 +82,14 @@ const showAlert = (content = null, time = 3000) => {
 const inputCheckBoxes = document.querySelectorAll("[type='checkbox']"); // Chọn tất cả các checkbox đúng cách
 
 //comment demo
-if (localStorage.getItem('voted') === 'true') {
-    window.location.href = 'block.html';
-} else {
-    setInterval(() => {
-        localStorage.setItem("voted", "true");
-        window.location.reload();
-    }, 5 * 60 * 1000);
-}
+// if (localStorage.getItem('voted') === 'true') {
+//     window.location.href = 'block.html';
+// } else {
+//     setInterval(() => {
+//         localStorage.setItem("voted", "true");
+//         window.location.reload();
+//     }, 5 * 60 * 1000);
+// }
 // End comment demo
 
 const checkboxIds = [];
@@ -192,7 +194,7 @@ if (buttonVote) {
             buttonConfirm.addEventListener("click", () => {
 
                 // Tăng biến count mỗi khi có một vote
-                incrementCount();
+                addUserVote();
 
                 //comment demo
                 localStorage.setItem('voted', 'true');
--- assets/js/ranking.js
+++ assets/js/ranking.js
@@ -2,26 +2,26 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getDatabase, ref, push, set, onValue, update, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
 //firebase config a Hung
-const firebaseConfig = {
-    apiKey: "AIzaSyDFIW4HVIG0usA868ltUJ_eTukLixdoT2A",
-    authDomain: "hcyu-fit.firebaseapp.com",
-    databaseURL: "https://hcyu-fit-default-rtdb.firebaseio.com",
-    projectId: "hcyu-fit",
-    storageBucket: "hcyu-fit.appspot.com",
-    messagingSenderId: "1059484411477",
-    appId: "1:1059484411477:web:83aca0a7b469d1fc5fd6dc"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDFIW4HVIG0usA868ltUJ_eTukLixdoT2A",
+//     authDomain: "hcyu-fit.firebaseapp.com",
+//     databaseURL: "https://hcyu-fit-default-rtdb.firebaseio.com",
+//     projectId: "hcyu-fit",
+//     storageBucket: "hcyu-fit.appspot.com",
+//     messagingSenderId: "1059484411477",
+//     appId: "1:1059484411477:web:83aca0a7b469d1fc5fd6dc"
+// };
 
 //My firebase config 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCAgJndV6rdgtlIpQ5cjuUrL9KSbCS6fTU",
-//     authDomain: "votting-app-52c6c.firebaseapp.com",
-//     databaseURL: "https://votting-app-52c6c-default-rtdb.firebaseio.com",
-//     projectId: "votting-app-52c6c",
-//     storageBucket: "votting-app-52c6c.appspot.com",
-//     messagingSenderId: "868756643395",
-//     appId: "1:868756643395:web:5abc86da00bdfac3d8fef6"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyCAgJndV6rdgtlIpQ5cjuUrL9KSbCS6fTU",
+    authDomain: "votting-app-52c6c.firebaseapp.com",
+    databaseURL: "https://votting-app-52c6c-default-rtdb.firebaseio.com",
+    projectId: "votting-app-52c6c",
+    storageBucket: "votting-app-52c6c.appspot.com",
+    messagingSenderId: "868756643395",
+    appId: "1:868756643395:web:5abc86da00bdfac3d8fef6"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -50,31 +50,33 @@ function groupDuplicates(arr) {
     return result;
 }
 
-let userCount = 0;
-
 // Hàm getCount với Promise để lấy userCount từ Firebase
 const getCount = () => {
     return new Promise((resolve, reject) => {
         const countRef = ref(db, 'voteCount');
 
         onValue(countRef, (snapshot) => {
-            if (snapshot.exists()) {
-                userCount = snapshot.val();
-                updateRanking();
-            } else {
-                reject("Vote count is not available.");
-            }
+            let userCounts = [];
+
+            snapshot.forEach((item) => {
+                const data = item.val();
+                userCounts.push(data.usersVote);
+            });
+
+            // Tính tổng số lượt vote
+            let totalVote = userCounts.length;
+            console.log(`quantity of vote: ${totalVote}`);
+
+            updateRanking(totalVote); // Truyền totalVote vào updateRanking
         });
     });
 };
 
 
-const updateRanking = () => {
+const updateRanking = (totalVote) => { // Nhận totalVote từ getCount
     onValue(performancesRef, (snapshot) => {
         let ids = [];
         let datas = [];
-
-        console.log(userCount);
 
         snapshot.forEach((item) => {
             const key = item.key;
@@ -86,9 +88,6 @@ const updateRanking = () => {
         // Group duplicates and sort by count
         const groupedNames = groupDuplicates(datas).sort((a, b) => b.count - a.count);
 
-        // Limit to the top 5 performers
-        // groupedNames = groupedNames.slice(0, 5);
-
         const listPerformer = document.querySelector("[list-performer]");
 
         // Lưu trữ vị trí hiện tại của từng thẻ <li> trước khi cập nhật
@@ -99,12 +98,12 @@ const updateRanking = () => {
 
         // Cập nhật HTML với dữ liệu mới
         const htmls = groupedNames.map(item => {
-            const percentage = (((item.count) / userCount) * 100).toFixed(2) >= 100 ? 100 : (((item.count) / userCount) * 100).toFixed(2);
+            const percentage = (((item.count) / totalVote) * 100).toFixed(2) >= 100 ? 100 : (((item.count) / totalVote) * 100).toFixed(2);
             return `
             <li class="performer-item" data-name="${item.name}" style="--progress-width: ${percentage}%;">
                 <form action="#" class="main__box-content--form">
                     <div class="item--award">
-                    <img src = "assets/images/award.png"/>
+                    <img src="assets/images/award.png"/>
                     </div>
                     <label class="performer--name">${item.name}</label>
                     <label class="quantity-vote">${percentage}%</label>
@@ -115,24 +114,20 @@ const updateRanking = () => {
 
         listPerformer.innerHTML = htmls;
 
-        console.log(groupedNames);
-
         // Tính toán vị trí mới của từng thẻ <li> và tạo animation mượt mà
         document.querySelectorAll('.performer-item').forEach((item) => {
             const name = item.dataset.name;
             const newPosition = item.getBoundingClientRect().top;
             const oldPosition = currentPositions[name];
 
-            // Nếu có sự thay đổi vị trí, áp dụng hiệu ứng di chuyển
             if (oldPosition !== undefined) {
                 anime({
                     targets: item,
-                    translateY: [oldPosition - newPosition, 0], // Di chuyển từ vị trí cũ đến vị trí mới
+                    translateY: [oldPosition - newPosition, 0],
                     duration: 1000,
                     easing: 'easeOutExpo'
                 });
             } else {
-                // Đối với các phần tử mới, làm hiệu ứng mờ dần vào
                 anime({
                     targets: item,
                     opacity: [0, 1],
@@ -142,12 +137,12 @@ const updateRanking = () => {
                 });
             }
         });
-
         console.log(groupedNames);
     });
 }
 
 getCount();
+
 
 
 
